3f5048e59e0519be0fefb715d8a8400bb9f6363b
Salvatorian <laurens.aarnoudse@gmail.com>
2016-03-07 00:55:23 +0100

    Added pre- and posttest links for FocusSessions The overview page now shows an iframe for the pretest of each session dynamically :D Header now has a dynamic link to the homepage Added a "detail' page for each session with a dynamic link to it from the overview page

--- README.markdown
+++ README.markdown
@@ -4,3 +4,8 @@ To add:
 V Links for pre and post test in the collection schema
 * Buttons after login that allow users to access sessions in the future
 * Make sure Doubles cant be added to DB
+* sign up page
+* general info section
+* different views for admin (who sees aprticipants) and normal users who only see their own sessions
+* create a userPage for each user where the admin can see which sessions they're attending and have attended
+* Add conditional logic that shows the pretest if the currenttime > starttime of the event and posttest if currenttime > 2 hours after starttime. Should prob also add links to the actual file just in case. Admin should be able to override these settings in case someone leaves early for instance
--- server/fixtures.js
+++ server/fixtures.js
@@ -5,8 +5,12 @@ if (FocusSessions.find().count() != 0) {
 if (FocusSessions.find().count() === 0) {
   FocusSessions.insert({
     type: 'normal',
-    iteration: 3,
+    iteration: 2,
     date: new Date("March 4, 2016 9:50:00"),
+    preTestLink: 'https://docs.google.com/forms/d/1ZanGWdNruYW1f46jUmGpL0TalLoZfRMVEMgdCDwPaJg/viewform',
+    postTestLink: 'https://docs.google.com/forms/d/1GNDI9TU8uAuMOWRGrcqkF-Y_JINmhBAx3xptX26drzA/viewform',
+   // <iframe src="https://docs.google.com/forms/d/1ZanGWdNruYW1f46jUmGpL0TalLoZfRMVEMgdCDwPaJg/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+
   }, function (error, result){
     console.log(error ? error.invalidKeys : result);
   });
